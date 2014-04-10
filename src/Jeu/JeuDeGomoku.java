1e03ec5b81e9a5b889feca870c6b02246c4a59c0
theodury <tdury2@gmail.com>
2014-04-10 09:47:53 +0200

    Fabrique partie (creation enum)

--- src/Jeu/JeuDeGomoku.java
+++ src/Jeu/JeuDeGomoku.java
@@ -1,4 +1,3 @@
-
 package Jeu;
 
 /**
@@ -6,5 +5,18 @@ package Jeu;
  * @author Mario
  */
 public class JeuDeGomoku {
-    
+
+    public JeuDeGomoku(TypePartie type) {
+        switch (type) {
+            case HumainVsHumain:
+                break;
+            case HumainVsAleatoire:
+                break;
+            case AleatoireVsAleatoire:
+                break;
+            default:
+                throw new AssertionError(type.name());
+        }
+
+    }
 }
