ed70a5b08358542bf92fc9c29569238fe0982f3f
theodury <tdury2@gmail.com>
2014-04-10 09:52:54 +0200

    javadoc

--- src/Plateau/Position.java
+++ src/Plateau/Position.java
@@ -2,19 +2,28 @@
 package Plateau;
 
 /**
- * Position
- * @author Théo Dury
+ * Position en fonction de la hauteur et la longueur du plateau
+ * @author TD
  */
 public class Position {
     
     int x;
     int y;
 
+    /**
+     * Construteur de position
+     * @param x valeur de la longueur
+     * @param y valeur de la hauteur
+     */
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * Affiche la position de x et y
+     * @return Retourne une chaine de caratere avec la position de x et y 
+     */
     @Override
     public String toString() {
         return "Position{" + "x=" + x + ", y=" + y + '}';
