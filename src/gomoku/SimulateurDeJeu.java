4572112342022ce3ff89fc2f6b6b3cd31c67c3c4
theodury <tdury2@gmail.com>
2014-04-16 11:50:42 +0200

    MenuGUI

---
+++ src/gomoku/SimulateurDeJeu.java
@@ -0,0 +1,21 @@
+package gomoku;
+
+
+import GUI.MenuGUI;
+
+
+/**
+ * Simulateur de jeu
+ *
+ * @author Théo Dury
+ */
+public class SimulateurDeJeu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuGUI play = new MenuGUI();  
+        play.setVisible(true);
+    }
+}
